0e9aebe6038fe5913e831dcfce0f319eb60a3966
istorp <sentoc@outlook.com>
2022-03-13 14:44:15 +0100

    Add project files.

---
+++ AndersEcommerce/Server/Data/DataContext.cs
@@ -0,0 +1,41 @@
+﻿namespace AndersEcommerce.Server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+
+                new Product
+                {
+                    Id = 1,
+                    Title = "Bipollen",
+                    Description = "Vår bipollen hjälper till med att öppna upp lungornas alveoler rejält.",
+                    ImageUrl = "https://fincasolmark.com/wp-content/uploads/2021/07/finca-solmark-bipollen-225g.jpg",
+                    Price = 150.50m
+                },
+             new Product
+             {
+                 Id = 2,
+                 Title = "Honung Avokado",
+                 Description = "Vår unika avokadohonung passar perfekt till rejäla ostar och att marinera köttbitar med.",
+                 ImageUrl = "https://fincasolmark.com/wp-content/uploads/2020/10/finca-solmark-honung-avokado-1.jpg",
+                 Price = 129.99m
+             },
+             new Product
+             {
+                 Id = 3,
+                 Title = "Honung Bergsblommor",
+                 Description = "Vår mest populära honung kommer från blommorna på berget ovanför gården.",
+                 ImageUrl = "https://fincasolmark.com/wp-content/uploads/2020/10/finca-solmark-honung-bergsblommor-1.jpg",
+                 Price = 89.99m
+             });
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ AndersEcommerce/Server/Migrations/20220313133028_ProduductSeeding.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AndersEcommerce.Server.Migrations
+{
+    public partial class ProduductSeeding : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[] { 1, "Vår bipollen hjälper till med att öppna upp lungornas alveoler rejält.", "https://fincasolmark.com/wp-content/uploads/2021/07/finca-solmark-bipollen-225g.jpg", 150.50m, "Bipollen" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[] { 2, "Vår unika avokadohonung passar perfekt till rejäla ostar och att marinera köttbitar med.", "https://fincasolmark.com/wp-content/uploads/2020/10/finca-solmark-honung-avokado-1.jpg", 129.99m, "Honung Avokado" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[] { 3, "Vår mest populära honung kommer från blommorna på berget ovanför gården.", "https://fincasolmark.com/wp-content/uploads/2020/10/finca-solmark-honung-bergsblommor-1.jpg", 89.99m, "Honung Bergsblommor" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
